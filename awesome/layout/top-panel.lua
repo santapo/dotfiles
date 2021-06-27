90ae432ee7023ca7fbe010b2885bef6272fc7b20
Santa Po <nguyentrongtuank62hust@gmail.com>
2021-06-28 06:00:00 +0700

    modulize layout

---
+++ awesome/layout/top-panel.lua
@@ -0,0 +1,59 @@
+local awful = require('awful')
+local menubar = require('menubar')
+local beautiful = require('beautiful')
+local wibox = require('wibox')
+
+
+local TagList = require('widgets.tag-list')
+local TaskList = require('widgets.task-list')
+
+terminal = "urxvt"
+mymainmenu = awful.menu({ items = { { "awesome", myawesomemenu, beautiful.awesome_icon },
+                                    { "open terminal", terminal }
+                                  }
+                        })
+
+mylauncher = awful.widget.launcher({ image = beautiful.awesome_icon,
+                                     menu = mymainmenu })
+
+menubar.utils.terminal = terminal
+
+mykeyboardlayout = awful.widget.keyboardlayout()
+
+mytextclock = wibox.widget.textclock()
+
+local TopPanel = function(s)
+  local panel = awful.wibar({ position = "top", screen = s })
+  
+  s.mypromptbox = awful.widget.prompt()
+  -- Create an imagebox widget which will contain an icon indicating which layout we're using.
+  -- We need one layoutbox per screen.
+  s.mylayoutbox = awful.widget.layoutbox(s)
+  s.mylayoutbox:buttons(awful.util.table.join(
+                          awful.button({ }, 1, function () awful.layout.inc( 1) end),
+                          awful.button({ }, 3, function () awful.layout.inc(-1) end),
+                          awful.button({ }, 4, function () awful.layout.inc( 1) end),
+                          awful.button({ }, 5, function () awful.layout.inc(-1) end)))
+
+  panel:setup{
+    layout = wibox.layout.align.horizontal,
+    { -- Left widgets
+        layout = wibox.layout.fixed.horizontal,
+        mylauncher,
+        TagList(s),
+        s.mypromptbox,
+    },
+    TaskList(s), -- Middle widget
+    { -- Right widgets
+        layout = wibox.layout.fixed.horizontal,
+        mykeyboardlayout,
+        wibox.widget.systray(),
+        mytextclock,
+        s.mylayoutbox,
+    },
+  }
+
+  return panel
+end
+
+return TopPanel
--- awesome/rc.lua
+++ awesome/rc.lua
@@ -27,38 +27,14 @@ terminal = "urxvt"
 editor = os.getenv("EDITOR") or "nvim"
 editor_cmd = terminal .. " -e " .. editor
 
+require('layout')
+require('configuration.client')
+require('configuration.tags')
+_G.root.keys(require('configuration.keys.global'))
 
--- {{{ Menu
--- Create a launcher widget and a main menu
--- myawesomemenu = {
---    { "hotkeys", function() hotkeys_popup.show_help(nil, awful.screen.focused()) end },
---    { "manual", terminal .. " -e man awesome" },
---    { "edit config", editor_cmd .. " " .. awesome.conffile },
---    { "restart", awesome.restart },
---    { "quit", function() awesome.quit() end },
--- }
-
-mymainmenu = awful.menu({ items = { { "awesome", myawesomemenu, beautiful.awesome_icon },
-                                    { "open terminal", terminal }
-                                  }
-                        })
-
-mylauncher = awful.widget.launcher({ image = beautiful.awesome_icon,
-                                     menu = mymainmenu })
-
--- Menubar configuration
-menubar.utils.terminal = terminal -- Set the terminal for applications that require it
--- }}}
-
--- Keyboard map indicator and switcher
-mykeyboardlayout = awful.widget.keyboardlayout()
-
--- {{{ Wibar
--- Create a textclock widget
-mytextclock = wibox.widget.textclock()
-       
-local function set_wallpaper(s)
-    -- Wallpaper
+awful.screen.connect_for_each_screen(
+  function(s)
+    -- If wallpaper is a function, call it with the screen
     if beautiful.wallpaper then
         local wallpaper = beautiful.wallpaper
         -- If wallpaper is a function, call it with the screen
@@ -67,54 +43,8 @@ local function set_wallpaper(s)
         end
         gears.wallpaper.maximized(wallpaper, s, true)
     end
-end
-
--- Re-set wallpaper when a screen's geometry changes (e.g. different resolution)
-screen.connect_signal("property::geometry", set_wallpaper)
-
-local TagList = require('widgets.tag-list')
-local TaskList = require('widgets.task-list')
-awful.screen.connect_for_each_screen(function(s)
-    -- Wallpaper
-    set_wallpaper(s)
-
-    -- Create a promptbox for each screen
-    s.mypromptbox = awful.widget.prompt()
-    -- Create an imagebox widget which will contain an icon indicating which layout we're using.
-    -- We need one layoutbox per screen.
-    s.mylayoutbox = awful.widget.layoutbox(s)
-    s.mylayoutbox:buttons(gears.table.join(
-                           awful.button({ }, 1, function () awful.layout.inc( 1) end),
-                           awful.button({ }, 3, function () awful.layout.inc(-1) end),
-                           awful.button({ }, 4, function () awful.layout.inc( 1) end),
-                           awful.button({ }, 5, function () awful.layout.inc(-1) end)))
-
-    -- Create the wibox
-    s.mywibox = awful.wibar({ position = "top", screen = s })
-    -- Add widgets to the wibox
-    s.mywibox:setup {
-        layout = wibox.layout.align.horizontal,
-        { -- Left widgets
-            layout = wibox.layout.fixed.horizontal,
-            mylauncher,
-            TagList(s),
-            s.mypromptbox,
-        },
-        TaskList(s), -- Middle widget
-        { -- Right widgets
-            layout = wibox.layout.fixed.horizontal,
-            mykeyboardlayout,
-            wibox.widget.systray(),
-            mytextclock,
-            s.mylayoutbox,
-        },
-    }
-end)
-
-
-require('configuration.client')
-require('configuration.tags')
-_G.root.keys(require('configuration.keys.global'))
+  end
+)
 
 -- {{{ Signals
 -- Signal function to execute when a new client appears.
