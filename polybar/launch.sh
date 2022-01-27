ccf20fd4ae3a072d7dd7ab70e0d563cf53ca0ba1
Santa Po <nguyentrongtuank62hust@gmail.com>
2022-01-28 03:27:24 +0700

    tagswitch

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,13 +1,35 @@
 #!/bin/bash
+(
+  flock 200
 
-killall -q polybar
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-polybar santapo &
+  killall -q polybar
 
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload santapo &
+  while pgrep -u $UID -x polybar > /dev/null; do sleep 0.5; done
+
+  outputs=$(xrandr --query | grep " connected" | cut -d" " -f1)
+  tray_output=eDP1
+  main_output=eDP1
+
+  for m in $outputs; do
+    if [[ $m == "HDMI1" ]]; then
+        tray_output=$m
+        main_output=$m
+    fi
   done
-else
-  polybar --reload santapo &
-fi
+
+  for m in $outputs; do
+    export MONITOR=$m
+    # export TRAY_POSITION=none
+    # if [[ $m == $tray_output ]]; then
+    #   TRAY_POSITION=right
+    # fi
+
+    if [[ $m == $main_output ]]; then
+      polybar --reload mainbar </dev/null >/var/tmp/polybar-$m.log 2>&1 200>&- &
+      disown
+    else
+      polybar --reload sidebar </dev/null >/var/tmp/polybar-$m.log 2>&1 200>&- &
+      disown
+    fi
+  done
+) 200>/var/tmp/polybar-launch.lock
