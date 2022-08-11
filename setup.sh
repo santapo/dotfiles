061c58064fc75767689d0f1a7cb695cabfaeb567
santapo <nguyentrongtuank62hust@gmail.com>
2022-08-11 22:50:11 +0700

    update setup script

--- setup.sh
+++ setup.sh
@@ -21,12 +21,43 @@ if [[ "$OSTYPE" == "Darwin"* ]]; then
 
     echo -e "${GREEN}Installing zsh-shell...${NC}"
     brew install zsh
+
     # change shell
     chsh -s $(which zsh)
+
+elif [[ "$OSTYPE" == "Linus"* ]]; then
+    mkdir -p $HOME/.local/bin/
+    echo -e "${GREEN}Installing NeoVim...${NC}"
+    wget https://github.com/neovim/neovim/releases/download/v0.7.2/nvim-linux64.tar.gz
+    tar xfz nvim-linux64.tar.gz
+    rm nvim-linux64.tar.gz
+    mv nvim-linux64/bin/nvim $HOME/.local/bin/
+
+    echo -e "${GREEN}Installing zsh-shell...${NC}"
+    wget https://sourceforge.net/projects/zsh/files/zsh/5.9/zsh-5.9.tar.xz
+    tar xf zsh-5.9.tar.xz
+    cd zsh-5.9
+    ./configure --prefix="$HOME/.local" \
+        CPPFLAGS="-I$HOME/.local/include" \
+        LDFLAGS="-L$HOME/.local/lib"
+    make -j && make install
+    # Append this to .bash_profile
+    # export PATH=$HOME/local/bin:$PATH
+    # export SHELL=`which zsh`
+    # [ -f "$SHELL" ] && exec "$SHELL" -l
 fi
 
 
 echo -e "${GREEN}Installing oh-my-zsh...${NC}"
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Install NeoVim Plugins
+# :PlugInstall
+# :CocInstall coc-tsserver coc-css coc-eslint coc-emmet coc-pyright coc-prettier
+# pip install black
+
+curl -sfLS install-node.vercel.app | bash -s -- --prefix=$HOME/.local --verbose
+
+
 # 
 
